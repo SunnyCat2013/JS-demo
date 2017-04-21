3009ef85f6fbe8ad03d11931d85999293e15893f
李振洋 <lizhenyang@raintai.com>
2017-04-21 22:18:57 +0800

    showing~

--- weather/src/containers/CurrentCity/index.jsx
+++ weather/src/containers/CurrentCity/index.jsx
@@ -10,11 +10,22 @@ import {
 
 class CurrentCity extends React.Component {
   render () {
-    console.log(this.props.cityInfos);
+    const { cityInfos } = this.props
     console.log('this.props in current city', this.props);
     return (
       <div>
         已添加的城市信息：
+        {
+          _.map(cityInfos, ({ id, name }) => {
+            return (
+              <div key={id}>
+                <span>
+                  名称：{name}
+                </span>
+              </div>
+            )
+          })
+        }
       </div>
     )
   }
